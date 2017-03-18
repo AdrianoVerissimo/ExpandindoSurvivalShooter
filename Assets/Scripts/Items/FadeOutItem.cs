fe0df60c284caf51bd1b2e58f5eeff35c87117a0
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-18 12:16:23 -0300

    Add comentários.

--- Assets/Scripts/Items/FadeOutItem.cs
+++ Assets/Scripts/Items/FadeOutItem.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FadeOutItem : MonoBehaviour {
 
-	public float timeBeforeFade = 2f;
-	public bool destroyAfterFade = true;
+	public float timeBeforeFade = 2f; //tempo antes de fazer o fade out
+	public bool destroyAfterFade = true; //se destruirá o objeto após o fade out
 
 	Animator anim;
 
@@ -13,17 +13,19 @@ public class FadeOutItem : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator>();
 
-		Invoke ("ActivateFade", timeBeforeFade);
+		Invoke ("ActivateFade", timeBeforeFade); //chama método de fade após um tempo
 	}
 
+	//ativa a animação de fade out
 	void ActivateFade()
 	{
 		anim.SetTrigger ("ItemFadeOut");
 	}
 
+	//destrói o objeto. Chamado automaticamente ao fim da animação de fade out no Animator deste objeto.
 	public void DestroyObject ()
 	{
-		if (destroyAfterFade)
+		if (destroyAfterFade) //marcou para destruir
 			Destroy (gameObject);
 	}
 }
