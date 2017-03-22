2d10b11c24304061027ba978bec1bd9cf190da70
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-21 23:56:13 -0300

    Adicionando sistema de auemnto de dificuldade gradual.

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -5,12 +5,15 @@ public class EnemyManager : MonoBehaviour
     public PlayerHealth playerHealth; //referência para energia do jogador
     public GameObject[] enemies; //referência para o inimigo que queremos dar spawn
     public float spawnTime = 3f; //tempo para dar spawn em um novo inimigo
+	public float spawnDecreaseTime = 0.2f;
+	public float timeBeforeDecreaseSpawn = 60f;
+	public float minSpawnTime = 1f;
     public Transform[] spawnPoints; //guarda os pontos de spawns
 
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime); //chama repetitivamente o método Spawn
+		CreateInvokes ();
     }
 
 	//cria o inimigo em um determinado lugar da fase
@@ -32,4 +35,20 @@ public class EnemyManager : MonoBehaviour
 		//instancia o inimigo
 		Instantiate (enemies[posArrayEnemies], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
+
+	void DecreaseSpawnTime()
+	{
+		spawnTime -= spawnDecreaseTime;
+		if (spawnTime < minSpawnTime)
+			spawnTime = minSpawnTime;
+
+		CancelInvoke ();
+		CreateInvokes ();
+	}
+
+	void CreateInvokes()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime); //chama repetitivamente o método Spawn
+		InvokeRepeating("DecreaseSpawnTime", timeBeforeDecreaseSpawn, timeBeforeDecreaseSpawn);
+	}
 }
